3c4f52f2461e52115bef7a3dcb67d5fc0dcfd82e
richardev29 <richardvn29@gmail.com>
2018-08-19 22:19:45 -0400

    routes file with Express Router

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const mongoose = require('mongoose')
 
+// Import Routes
+const user = require('./routes/api/user')
+const posts = require('./routes/api/posts')
+const profile = require('./routes/api/profile')
+
 // DB config
 const db = require('./config/keys').mongoURI
 
@@ -9,11 +14,15 @@ mongoose.connect(db)
   .then(() => console.log('DB is connected'))
   .catch(err => console.log(err))
 
-const app = express()
+const app = express() 
 
+// Route config
+app.get('/', (req, res) => res.send('hello'))
+app.use('/api/user', user)
+app.use('/api/posts', posts)
+app.use('/api/profile', profile)
 
 // Port Config 
 const port = process.env.PORT || 5000
 app.listen(port, () => console.log(`Server is running on ${port}`))
 
-app.get('/', (req, res) => res.send('hello'))
