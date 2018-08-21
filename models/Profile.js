97a9ee31b80866e62daa91267dc4aeb0edbccd7e
richardev29 <richardvn29@gmail.com>
2018-08-20 23:05:31 -0400

    create a Profile model

---
+++ models/Profile.js
@@ -0,0 +1,68 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const ProfileSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'users'
+  },
+  handle: {
+    type: String,
+    required: true,
+    max: 40
+  },
+  company: {
+    type: String
+  },  
+  bio: {
+    type: String
+  },
+  skills: {
+    type: String
+  },
+  githubAcc: {
+    type: String
+  },
+  experiences: [
+    {
+      title: {
+        type: String,
+        require: true
+      },
+      company: {
+        type: String,
+        require: true
+      },
+      jobDesc: {
+        type: String
+      },
+      from: {
+        type: Date, 
+        require: true
+      },
+      to: {
+        type: Date
+      }
+    }
+  ],
+  social: {
+    youtube: {
+      type: String
+    },
+    linkedin: {
+      type: String
+    },
+    facebook: {
+      type: String
+    },
+    twitter: {
+      type: String
+    },
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+})
+
+module.exports = Profile = mongoose.model('profile', ProfileSchema)
