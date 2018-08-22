85b21c016cf63734cde4ce18f65823803e33eb75
richardev29 <richardvn29@gmail.com>
2018-08-21 22:26:36 -0400

    add State to Login

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,22 +1,57 @@
 import React, { Component } from 'react'
 
 class Login extends Component {
+  constructor(){
+    super()
+    this.state = {
+      email: '',
+      password: ''
+    }
+  }
+  onChange = (e) => {
+    this.setState({[e.target.name]: e.target.value})
+  }
+  onSubmit = (e) => {
+    e.preventDefault()
+    const {email, password} = this.state
+    const user = {
+      email: email,
+      password: password
+    }
+    console.log(user)
+  }
   render(){
     return(
-      <div class="login">
-        <div class="container">
-          <div class="row">
-            <div class="col-md-8 m-auto">
-              <h1 class="display-4 text-center">Log In</h1>
-              <p class="lead text-center">Sign in to your DevConnector account</p>
-              <form action="dashboard.html">
-                <div class="form-group">
-                  <input type="email" class="form-control form-control-lg" placeholder="Email Address" name="email" />
+      <div className="login">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-8 m-auto">
+              <h1 className="display-4 text-center">Log In</h1>
+              <p className="lead text-center">Sign in to your DevConnector account</p>
+              <form onSubmit={this.onSubmit}>
+                <div className="form-group">
+                  <input 
+                    type="email" 
+                    className="form-control form-control-lg" 
+                    placeholder="Email Address" 
+                    name="email" 
+                    value={this.state.email}
+                    onChange={this.onChange}
+                    required
+                  />
                 </div>
-                <div class="form-group">
-                  <input type="password" class="form-control form-control-lg" placeholder="Password" name="password" />
+                <div className="form-group">
+                  <input 
+                    type="password" 
+                    className="form-control form-control-lg" 
+                    placeholder="Password" 
+                    name="password" 
+                    value={this.state.password}
+                    onChange={this.onChange}
+                    required
+                  />
                 </div>
-                <input type="submit" class="btn btn-info btn-block mt-4" />
+                <input type="submit" className="btn btn-info btn-block mt-4" />
               </form>
             </div>
           </div>
