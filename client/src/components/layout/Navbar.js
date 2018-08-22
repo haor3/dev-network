f2cbf528725dc7ea7211155eaed873540a7ee522
richardev29 <richardvn29@gmail.com>
2018-08-21 21:39:31 -0400

    modified Navbar

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-
+import {Link} from 'react-router-dom'
 class Navbar extends Component {
   render(){
     return(
@@ -14,17 +14,17 @@ class Navbar extends Component {
             <div className="collapse navbar-collapse" id="mobile-nav">
               <ul className="navbar-nav mr-auto">
                 <li className="nav-item">
-                  <a className="nav-link" href="profiles.html"> Developers
-                  </a>
+                  <Link className="nav-link" to="/profiles"> Profiles
+                  </Link>
                 </li>
               </ul>
 
               <ul className="navbar-nav ml-auto">
                 <li className="nav-item">
-                  <a className="nav-link" href="register.html">Sign Up</a>
+                  <Link className="nav-link" to="/register">Register</Link>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link" href="login.html">Login</a>
+                  <Link className="nav-link" to="/login">Login</Link>
                 </li>
               </ul>
             </div>
