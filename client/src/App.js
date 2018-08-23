869d958d211a8a43552dfce6554210faa7168329
richardev29 <richardvn29@gmail.com>
2018-08-23 17:09:36 -0400

    check the Authentication after Login

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom'
-
 import Navbar from './components/layout/Navbar'
 import Footer from './components/layout/Footer'
 import Landing from './components/layout/Landing'
 import Register from './components/auth/Register'
 import Login from './components/auth/Login'
+import jwt_decode from 'jwt-decode'
+import setAuthToken from './utils/setAuthToken'
+import { setCurrentUser } from './actions/user.actions'
+import store from './store'
 
 import './App.css';
+
+ 
+if(localStorage.jwtToken){
+  setAuthToken(localStorage.jwtToken)
+  const decoded = jwt_decode(localStorage.jwtToken)
+  store.dispatch(setCurrentUser(decoded))
+}
+
+
 class App extends Component {
   render() {
     return (
