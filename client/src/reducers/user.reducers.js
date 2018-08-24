5aa0276bd154d1023c78a61c456e9fadc4024677
richardev29 <richardvn29@gmail.com>
2018-08-24 19:24:20 -0400

    remove error types

--- client/src/reducers/user.reducers.js
+++ client/src/reducers/user.reducers.js
@@ -1,19 +1,17 @@
 import {
   LOADING_USER_DATABASE,
   USER_CREATED_SUCCESS,
-  USER_CREATED_FAIL,
-  USER_LOGIN_FAIL,
-  USER_LOGIN_SUCCESS
+  USER_LOGIN_SUCCESS,
+  USER_LOG_OUT
 } from '../actions/types'
 
-const userState = {
+const initialState = {
   isLoading: false,
   isAuthenticated: false,
-  user: {},
-  errors: {}
+  user: {}
 }
 
-export const registerState = (state=userState, action={}) => {
+export const registerState = (state=initialState, action={}) => {
   switch(action.type){
     case LOADING_USER_DATABASE:
       return{...state, 
@@ -24,32 +22,26 @@ export const registerState = (state=userState, action={}) => {
               isLoading: false,
               user: action.payload
             }
-    case USER_CREATED_FAIL:
-      return{...state, 
-              isLoading: false,
-              errors: action.payload
-            }
     default:
       return state
   }
 }
 
-export const loginState = (state=userState, action={}) => {
+export const loginState = (state=initialState, action={}) => {
   switch(action.type){
     case LOADING_USER_DATABASE:
       return{...state, 
               isLoading: true
             }
     case USER_LOGIN_SUCCESS:
-      return{...state, 
-              isLoading: false,
+      return{...state,
               isAuthenticated: true,
               user: action.payload
             }
-    case USER_LOGIN_FAIL:
-      return{...state, 
-              isLoading: false,
-              errors: action.payload
+    case USER_LOG_OUT:
+      return{...state,
+              isAuthenticated: false,
+              user: action.payload
             }
     default:
       return state
