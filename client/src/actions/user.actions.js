d08d06fce8cb5339fd3254d1cf2031b300aa61be
richardev29 <richardvn29@gmail.com>
2018-08-24 19:29:50 -0400

    add logout action

--- client/src/actions/user.actions.js
+++ client/src/actions/user.actions.js
@@ -3,25 +3,28 @@ import {
   USER_CREATED_SUCCESS,
   USER_CREATED_FAIL,
   USER_LOGIN_FAIL,
-  USER_LOGIN_SUCCESS
+  USER_LOGIN_SUCCESS,
+  USER_LOG_OUT
 } from './types'
 import jwt_decode from 'jwt-decode'
 import setAuthToken from '../utils/setAuthToken'
 const axios = require('axios')
 
-export const registerUser = userData => dispatch => {
+// user register 
+export const registerUser = (userData, history) => dispatch => {
   dispatch({type: LOADING_USER_DATABASE})
   axios.post('/api/user/register', userData)
       .then(user => dispatch({
                               type: USER_CREATED_SUCCESS, 
                               payload: user.data
                             }))
+      .then(() => history.push('/login'))
       .catch(err => dispatch({
                               type: USER_CREATED_FAIL, 
                               payload: err.response.data
                             }))
 }
-
+//user log in
 export const loginUser = userData => dispatch => {
   dispatch({
             type: LOADING_USER_DATABASE
@@ -43,10 +46,22 @@ export const loginUser = userData => dispatch => {
                             payload: err.response.data
                           }))
 }
-
+// set current user
 export const setCurrentUser = decoded => {
   return {
     type: USER_LOGIN_SUCCESS, 
     payload: decoded
   }
+}
+// user log out
+export const logoutUser = () => dispatch => {
+  // remove the token
+  localStorage.removeItem('jwtToken')
+  // remove auth header
+  setAuthToken(false)
+  // set current user to false
+  dispatch({
+    type: USER_LOG_OUT,
+    payload: {}
+  })
 }
