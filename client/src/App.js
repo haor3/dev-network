5e23b7f1ff021e8ff960b9b315131c97584869fa
richardev29 <richardvn29@gmail.com>
2018-08-24 20:24:26 -0400

    clear profile after expiration

--- client/src/App.js
+++ client/src/App.js
@@ -9,21 +9,28 @@ import Dashboard from './components/layout/Dashboard'
 import jwt_decode from 'jwt-decode'
 import setAuthToken from './utils/setAuthToken'
 import { setCurrentUser, logoutUser } from './actions/user.actions'
+import {clearProfile} from './actions/profile.actions'
 import store from './store'
 
 import './App.css';
  
 if(localStorage.jwtToken){
+  // set Auth token header 
   setAuthToken(localStorage.jwtToken)
+  // decode token to get user info and exp
   const decoded = jwt_decode(localStorage.jwtToken)
+  // set user isAuthentiated
   store.dispatch(setCurrentUser(decoded))
+  // check for expired token
+  const currentTime = Date.now()/1000
+  if(decoded.exp < currentTime){
+    // log out user
+    store.dispatch(logoutUser())
+    // clear the user profile
+    store.dispatch(clearProfile())
+  }
 }
-// check for expired token
-const currentTime = Date.now()/1000
-if(decoded.exp < currentTime){
-  // log out user
-  store.dispatch(logoutUser())
-}
+
 
 
 class App extends Component {
