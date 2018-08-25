4daa1c1a1bcace573e1b97468f35493488796180
richardev29 <richardvn29@gmail.com>
2018-08-24 20:09:02 -0400

    add Profile reducer to store

--- client/src/store.js
+++ client/src/store.js
@@ -2,8 +2,9 @@ import { createStore, applyMiddleware, combineReducers, compose } from 'redux'
 import thunkMiddleware from 'redux-thunk'
 import { registerState, loginState } from './reducers/user.reducers'
 import { errorState } from './reducers/error.reducers'
+import {profileState} from './reducers/profile.reducers'
 
-const rootReducer = combineReducers({registerState, loginState, errorState})
+const rootReducer = combineReducers({registerState, loginState, errorState, profileState})
 const store = createStore(
                 rootReducer, 
                 compose(applyMiddleware(thunkMiddleware),
