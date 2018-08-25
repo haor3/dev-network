27f852cd57c16d435b039b553362d2c1320bfb8b
richardev29 <richardvn29@gmail.com>
2018-08-24 20:08:28 -0400

    add profile types

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -1,8 +1,13 @@
+// register new user
 export const LOADING_USER_DATABASE = 'LOADING_USER_DATABASE'
 export const USER_CREATED_SUCCESS = 'USER_CREATED_SUCCESS'
 export const USER_CREATED_FAIL = 'USER_CREATED_FAIL'
-
+// user log in
 export const USER_LOGIN_SUCCESS = 'USER_LOGIN_SUCCESS'
 export const USER_LOGIN_FAIL = 'USER_LOGIN_FAIL'
-
+// user log out
 export const USER_LOG_OUT = 'USER_LOG_OUT'
+// profile
+export const PROFILE_LOADING = 'PROFILE_LOADING'
+export const PROFILE_NOT_FOUND = 'PROFILE_NOT_FOUND'
+export const PROFILE_FOUND = 'PROFILE_FOUND'
