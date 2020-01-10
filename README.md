5d24e8780449bf1759fa85aff21f81be51e800f3
haobui <iamhaobui@gmail.com>
2020-01-10 17:09:02 -0500

    refactor

--- README.md
+++ README.md
@@ -1,9 +1,6 @@
-build a web app with React-Redux, using Express, JWT, Passport and MongoDB.<br>
-Just finish the login and create profile page.<br>
-Works still need to be worked on.<br>
-You can test the app on https://damp-brushlands-60893.herokuapp.com/
++ A web app with React, Redux, Express, JWT, Passport and MongoDB
++ It has the login, register and create profile page
 
-**
+# Run the app
 1. npm install
-2. npm run client-install
-3. npm start
+2. npm start
--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -41,17 +41,28 @@ class Navbar extends Component {
         <nav className="navbar navbar-expand-sm navbar-dark bg-dark mb-4">
           <div className="container">
             <Link className="navbar-brand" to="/">DevConnector</Link>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#mobile-nav">
-              <span className="navbar-toggler-icon"></span>
-            </button>
+            {
+              isAuthenticated && 
+              (
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#mobile-nav">
+                  <span className="navbar-toggler-icon"></span>
+                </button>
+              )
+            }       
 
             <div className="collapse navbar-collapse" id="mobile-nav">
-              <ul className="navbar-nav mr-auto">
-                <li className="nav-item">
-                  <Link className="nav-link" to="/profiles"> Profiles
-                  </Link>
-                </li>
-              </ul>     
+              {
+                isAuthenticated && 
+                (
+                  <ul className="navbar-nav mr-auto">
+                    <li className="nav-item">
+                      <Link className="nav-link" to="/profiles"> Profiles
+                      </Link>
+                    </li>
+                  </ul>
+                )
+              }
+                  
                 
              
             </div>
